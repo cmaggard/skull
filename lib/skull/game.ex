9c72052d7fb685a7186ea67d19a9465a9231644e
Cody Maggard <cody.maggard@gmail.com>
2021-09-21 04:58:14 +0000

    Beginning of Skull game object logic

---
+++ lib/skull/game.ex
@@ -0,0 +1,26 @@
+defmodule Skull.Game do
+  defstruct players: []
+
+  def new_game do
+    %__MODULE__{}
+  end
+
+  def add_player(game, [name: name, pid: pid, ref: ref]) do
+    cond do
+      player_count(game) == 6 ->
+        {{:error, :player_count_exceeded}, game}
+      true ->
+        game = %{game | players: [{ref, pid, name} | game.players]}
+        {:ok, game}
+    end
+  end
+
+  def remove_player(game, ref) do
+    new_players = Enum.reject(game.players, fn {p_ref, _pid, _name} -> ref == p_ref end)
+    {:ok, %{game | players: new_players }}
+  end
+
+  def player_count(game) do
+    length game.players
+  end
+end
---
+++ test/skull/game_test.exs
@@ -0,0 +1,47 @@
+defmodule Skull.GameTest do
+  use ExUnit.Case
+
+  alias Skull.Game
+
+  describe "new_game/0" do
+    test "builds a new game" do
+      assert is_struct(Game.new_game(), Game)
+    end
+  end
+
+  describe "add_player/2" do
+    test "it adds a player to the game" do
+      {:ok, game} = Game.new_game()
+               |> add_player()
+
+      assert Game.player_count(game) == 1
+    end
+
+    test "adding another player when there are six players fails" do
+      game = Game.new_game()
+      game = Enum.reduce(1..6, game, fn player, acc ->
+        {:ok, game} = add_player(acc, [name: "Player " <> Integer.to_string(player)])
+        game
+      end)
+      {result, _game} = add_player(game)
+      assert result == {:error, :player_count_exceeded}
+    end
+  end
+
+  describe "remove_player/2" do
+    test "it removes a player from the game" do
+      ref = make_ref()
+      {:ok, game} = Game.new_game()
+                    |> add_player([ref: ref])
+      {:ok, game} = Game.remove_player(game, ref)
+      assert Game.player_count(game) == 0
+    end
+  end
+
+  defp add_player(game, args \\ []) do
+    name = Keyword.get(args, :name, "Cody")
+    pid = Keyword.get(args, :pid, self())
+    ref = Keyword.get(args, :ref, make_ref())
+    Game.add_player(game, name: name, pid: pid, ref: ref)
+  end
+end
