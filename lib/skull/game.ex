a783bd11274b8c8e441b4fcb984c9b7c04c9deaa
Cody Maggard <cody.maggard@gmail.com>
2021-09-27 20:58:27 +0000

    Function to initialize player states on game start- needs refactor maybe?

--- lib/skull/game.ex
+++ lib/skull/game.ex
@@ -1,9 +1,12 @@
 defmodule Skull.Game do
   alias Skull.Game.Query
+  alias Skull.PlayerState
 
   def new_game do
     %{
-      players: %{}
+      players: %{},
+      player_order: [],
+      current_player: nil
     }
   end
 
@@ -20,6 +23,23 @@ defmodule Skull.Game do
 
   def remove_player(game, id) do
     {_player, game} = pop_in(game, [:players, id])
+    {:ok, game}
+  end
+
+  def build_player_states(game) do
+    player_state =
+      Map.keys(game.players)
+      |> Enum.reduce(%{}, fn player, acc ->
+        Map.put(acc, player, PlayerState.build_state())
+      end)
+
+    {:ok, Map.put(game, :player_state, player_state)}
+  end
+
+  def randomize_player_order(game) do
+    players = Map.keys(game.players)
+    randomized = Enum.shuffle(players)
+    game = %{game | player_order: randomized, current_player: Enum.at(randomized, 0)}
     {:ok, game}
   end
 end
--- lib/skull/game/query.ex
+++ lib/skull/game/query.ex
@@ -4,4 +4,12 @@ defmodule Skull.Game.Query do
   def player_count(game) do
     map_size(game.players)
   end
+
+  def get_player_ids(game) do
+    Map.keys(game.players)
+  end
+
+  def get_player_state(game, player_id) do
+    game.player_states[player_id]
+  end
 end
